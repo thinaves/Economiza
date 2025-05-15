91b8bd61d92b914dbaa12f76a60dd4c087740b01
Sergiosoid <sergiosoud@outlook.com.br>
2025-05-15 20:15:24 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
 import sqlite3
+import mysql.connector
 
 app = Flask(__name__)
 
+# Função para conectar ao banco de dados MySQL
+def conectar_banco():
+    return mysql.connector.connect(
+        host='35.199.108.87' , # IP da instância do Cloud SQL
+        port = 3306,
+        user = root
 # Rota de teste
 @app.route('/')
 def home():
@@ -31,4 +38,4 @@ def receber_nota():
 
 # Rodar o app apenas se for o arquivo principal
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)    app.run(debug=True, host='35.199.108.87', port=3306)
